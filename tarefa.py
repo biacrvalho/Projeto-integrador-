32f71b015baa378a6fb42ec3d55bc732ee6e50dd
biacrvalho <160725144+biacrvalho@users.noreply.github.com>
2024-03-20 09:52:35 -0300

    Add files via upload

---
+++ tarefa.py
@@ -0,0 +1,26 @@
+a = 1
+b = 2
+c = 3
+d = 4
+frase = ("A aula foi boa")
+quantidade_espaços = frase.count(" ")
+print ("a quantidade de espaços na frase é", quantidade_espaços)
+palavra = ("Beatriz")
+quantidade_letras = len("Beatriz")
+print ("a quantidade de letras é", quantidade_letras)
+média = (a + b + c + d) / 4
+print ("a média é", média)
+quadrado = d * d
+print ("o quadrado é", quadrado) 
+dobro = d + d
+print ("o dobro é", dobro)
+if a > b:
+ print ("a é maior que b.")
+else:
+ print ("b é maior que a.")
+maior_número = max (a, b , c, d)
+print ("o maior número é", maior_número)
+
+    
+
+
